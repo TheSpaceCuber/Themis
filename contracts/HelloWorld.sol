7177d71c470633028b187ead695f8c670e48b021
alanlowzies <30099983+alanlowzies@users.noreply.github.com>
2023-03-27 15:27:16 +0800

    HelloWorld.sol: Experiment with events

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -3,12 +3,15 @@ pragma solidity ^0.5.17;
 contract HelloWorld {
     string message;
 
+    event HelloSaid(address saidBy);
+
     constructor() public {
         message = "Hello World!";
     }
 
     function setMessage(string memory _message) public {
         message = _message;
+        emit HelloSaid(msg.sender);
     }
 
     function getMessage() public view returns (string memory) {
