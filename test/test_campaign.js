935b63d10db4e32a9820e0d15798b3f9e357ed34
tjiarong <jia8991@gmail.com>
2023-04-08 18:58:47 +0800

    All test cases

--- test/test_campaign.js
+++ test/test_campaign.js
@@ -5,16 +5,15 @@ const oneEth = new BigNumber(1000000000000000000); // 1 eth
 
 var assert = require("assert");
 var IAM = artifacts.require("../contracts/IAM.sol");
-var Campaign = artifacts.require("../contracts/Campaign.sol");
-var CampaignFactory = artifacts.require("../contracts/CampaignFactory.sol");
+var Campaign = artifacts.require("../contracts/TestCampaign.sol");
+var CampaignFactory = artifacts.require("../contracts/TestCampaignFactory.sol");
 
 contract ("Campaign", function(accounts){
     before( async() => {
         IAMInstance = await IAM.deployed();
         campaignFactoryInstance = await CampaignFactory.deployed();
     });
-    
-    console.log("Testing IAM contract");
+
     let camp1;
 
     // Using emitted events to track status
@@ -51,16 +50,27 @@ contract ("Campaign", function(accounts){
         );
     });
 
-    console.log("Testing CampaignFactory contract");
+    it("CAMFAC01: Add campaign while account status not verified. [Pass]", async() => {
+        await truffleAssert.reverts(
+            campaignFactoryInstance.addCampaign({from: accounts[0]}),
+            "Address is not verified"
+        );
+        await IAMInstance.setLocked(accounts[0]);
+        await truffleAssert.reverts(
+            campaignFactoryInstance.addCampaign({from: accounts[0]}),
+            "Address is not verified"
+        );
+    });
 
-    it("CAMF01: Create Campaign with addCampaign() [Pass]", async() => {
+    it("CAMFAC02: Create campaign with addCampaign() [Pass]", async() => {
         await IAMInstance.setVerified(accounts[0]);
         let addC = await campaignFactoryInstance.addCampaign({from: accounts[0]});
+        // Save campaign address in variable for future test case
         camp1 = await addC['logs'][0]['args'][1];
         truffleAssert.eventEmitted(addC, 'mountCampaign');
     });
 
-    it("CAMF02: Create 5 more campaigns with addCampaign() [Fail]", async() => {
+    it("CAMFAC03: Create 5 more campaigns with addCampaign() [Fail]", async() => {
         // Create 4 campaign to hit limit
         await campaignFactoryInstance.addCampaign({from: accounts[0]});
         await campaignFactoryInstance.addCampaign({from: accounts[0]});
@@ -74,13 +84,13 @@ contract ("Campaign", function(accounts){
         );
     });
 
-    it("CAMF03: Create campaign with addCampaign(uint16 durationHrs) [Pass]", async() => {
+    it("CAMFAC04: Create campaign with addCampaign(uint16 durationHrs) [Pass]", async() => {
         await IAMInstance.add(accounts[1]);
         let addC3 = await campaignFactoryInstance.methods['addCampaign(uint16)'](240, {from: accounts[1]});
         truffleAssert.eventEmitted(addC3, 'mountCampaign');
     });
 
-    it("CAMF04: Create campaign with addCampaign(uint16 durationHrs) using invalid date [Fail]", async() => {
+    it("CAMFAC05: Create campaign with addCampaign(uint16 durationHrs) using invalid date [Fail]", async() => {
         // Non uint16 number
         await truffleAssert.fails(
             campaignFactoryInstance.methods['addCampaign(uint16)'](-999, {from: accounts[1]})
@@ -104,7 +114,7 @@ contract ("Campaign", function(accounts){
         );
     });
 
-    it("CAMF05: Create 5 more Campaigns with addCampaign(uint16 durationHrs) [Fail]", async() => {
+    it("CAMFAC06: Create 5 more Campaigns with addCampaign(uint16 durationHrs) [Fail]", async() => {
         // Create 4 campaign to hit limit
         await campaignFactoryInstance.methods['addCampaign(uint16)'](240, {from: accounts[1]});
         await campaignFactoryInstance.methods['addCampaign(uint16)'](240, {from: accounts[1]});
@@ -118,41 +128,67 @@ contract ("Campaign", function(accounts){
         );
     });
     
-    console.log("Testing Campaign contract");
-
     it("CAM01: Donate to campaign [Pass]", async() => {
         await IAMInstance.add(accounts[2]);
         campaignInstance = await Campaign.at(camp1);
-        let donate = await campaignInstance.donate({from: accounts[2], value: 10});
+        let donate = await campaignInstance.donate(false, {from: accounts[2], value: oneEth});
         truffleAssert.eventEmitted(donate, 'donationMade'); 
     });
 
     it("CAM02: Donate to campaign with invalid value [Fail]", async() => {
         await truffleAssert.reverts(
-            campaignInstance.donate({from: accounts[2], value: 0}),
+            campaignInstance.donate(false, {from: accounts[2], value: 0}),
             "Invalid donation amount"
         );
-
+        
+        // Over user's balance
         await truffleAssert.fails(
-            campaignInstance.donate({from: accounts[2], value: oneEth.multipliedBy(1000)})
+            campaignInstance.donate(false, {from: accounts[2], value: oneEth.multipliedBy(1000)})
         );
     });
 
     it("CAM03: Owner withdraw campaign before due date [Fail]", async() => {
         await truffleAssert.reverts(
-            campaignInstance.withdraw({from: accounts[0]}),
+            campaignInstance.withdraw(false, {from: accounts[0]}),
             "Campaign is ongoing"
         );
     });
 
     it("CAM04: Not Owner withdraw campaign [Fail]", async() => {
         await truffleAssert.reverts(
-            campaignInstance.withdraw({from: accounts[1]}),
+            campaignInstance.withdraw(false, {from: accounts[1]}),
             "Caller is not owner"
         );
     });
 
     it("CAM05: Owner withdraw campaign [Pass]", async() => {
+        // Balance casted to Bigint for comparison
+        let accBal = await web3.eth.getBalance(accounts[0]);
+        accBal = await BigInt(accBal);
+        let CFBal = await web3.eth.getBalance(campaignFactoryInstance.address);
+        CFBal = await BigInt(CFBal);
+        let dAmount = await campaignInstance.getTotalDonated();
+        dAmount = await BigInt(dAmount);
         
+        let commission = await dAmount / 10n ;
+        let netDonationAmt = await dAmount - commission;
+
+        let withdrawn = await campaignInstance.withdraw(true, {from: accounts[0]});
+        truffleAssert.eventEmitted(withdrawn, 'hasWithdrawn'); 
+        
+        let newAccBal = await web3.eth.getBalance(accounts[0]);
+        newAccBal = await BigInt(newAccBal);
+        let newCFBal = await web3.eth.getBalance(campaignFactoryInstance.address);
+        newCFBal = await BigInt(newCFBal);
+
+        await assert.equal(
+            (accBal + netDonationAmt) / 100000000000000000n,
+            newAccBal / 100000000000000000n
+        );
+
+        await assert.equal(
+            (CFBal + commission) / 100000000000000000n,
+            newCFBal / 100000000000000000n
+        );
     });
 });
