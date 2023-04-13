db7b2d57615adf4042b82f863e8f6bcbc5dcd73e
Teo Jia Rong <69455499+tjiarong@users.noreply.github.com>
2023-04-14 03:39:30 +0800

    Unit test update (#14)
    * Update
    
    * Update
    
    * Update
    
    * Added comments to clarify certain portion
    
    * Fix Typo
    
    * Fix event name
    
    * Commented out redundant add() call

--- test/test_campaign.js
+++ test/test_campaign.js
@@ -14,15 +14,14 @@ contract ("Campaign", function(accounts){
         campaignFactoryInstance = await CampaignFactory.deployed();
     });
 
-    // Using emitted events to track status
-    it("IAM01-1: Registering of beneficiary [Pass]", async() => {
-        let setDist = await IAMInstance.add(accounts[0]);
+    it("IAM01: Registering of beneficiary [Pass]", async() => {
         // Beneficiary status should be verified upon add
+        let setDist = await IAMInstance.add(accounts[0]);
+        
+        // Using emitted events to track status
         truffleAssert.eventEmitted(setDist, 'AddVerifiedOrg'); 
-    });
-
-    // Using getStatus to track status
-    it("IAM01-2: Registering of beneficiary [Pass]", async() => {
+        
+        // Using getStatus to track status
         let bStatus = await IAMInstance.getStatus(accounts[0]);
         await assert.equal(
             bStatus.toString(),
@@ -48,11 +47,49 @@ contract ("Campaign", function(accounts){
         );
     });
 
-    it("CAMFAC01: Add campaign while account status not verified. [Pass]", async() => {
-        await truffleAssert.reverts(
-            campaignFactoryInstance.addCampaign({from: accounts[0]}),
-            "Address is not verified"
+    it("IAM04: Set verified for a beneficiary [Pass]", async() => {
+        await IAMInstance.setVerified(accounts[0]);
+        let bStatus = await IAMInstance.getStatus(accounts[0]);
+        await assert.equal(
+            bStatus.toString(),
+            1 // status.VERIFIED
         );
+    });
+
+    it("IAM05: Set status of non-registered beneficiary [Fail]", async() => {
+        // Attempt to set the status for a non-registered beneficiary
+        await truffleAssert.reverts(
+            IAMInstance.setDistrust(accounts[1]),
+            "Organisation address does not exist"
+        );
+
+        await truffleAssert.reverts(
+            IAMInstance.setLocked(accounts[1]),
+            "Organisation address does not exist"
+        );
+    });
+
+    // Attempt to registered a beneficiary that has already registered.
+    it("IAM06: Registering of a registered beneficiary [Fail]", async() => {
+        // Attempt to register account with different status
+        await IAMInstance.setVerified(accounts[0]);
+        await truffleAssert.reverts(
+            IAMInstance.add(accounts[0]),
+            "Organisation address already exists"
+        );
+        await IAMInstance.setLocked(accounts[0]);
+        await truffleAssert.reverts(
+            IAMInstance.add(accounts[0]),
+            "Organisation address already exists"
+        );
+        await IAMInstance.setDistrust(accounts[0]);
+        await truffleAssert.reverts(
+            IAMInstance.add(accounts[0]),
+            "Organisation address already exists"
+        );
+    });
+
+    it("CAMFAC01: Add campaign while account status not verified. [Pass]", async() => {
         await IAMInstance.setLocked(accounts[0]);
         await truffleAssert.reverts(
             campaignFactoryInstance.addCampaign({from: accounts[0]}),
@@ -128,19 +165,21 @@ contract ("Campaign", function(accounts){
     });
     
     it("CAM01: Donate to campaign [Pass]", async() => {
-        await IAMInstance.add(accounts[2]);
+        // await IAMInstance.add(accounts[2]);
+        //Initialise campaign instance using previously saved address of a campaign
         campaignInstance = await Campaign.at(camp1);
         let donate = await campaignInstance.donate(false, {from: accounts[2], value: oneEth});
         truffleAssert.eventEmitted(donate, 'DonationMade'); 
     });
 
     it("CAM02: Donate to campaign with invalid value [Fail]", async() => {
+        // Invalid amount
         await truffleAssert.reverts(
             campaignInstance.donate(false, {from: accounts[2], value: 0}),
             "Invalid donation amount"
         );
         
-        // Over user's balance
+        // Donation amount over user's balance
         await truffleAssert.fails(
             campaignInstance.donate(false, {from: accounts[2], value: oneEth.multipliedBy(1000)})
         );
@@ -162,6 +201,7 @@ contract ("Campaign", function(accounts){
 
     it("CAM05: Owner withdraw campaign [Pass]", async() => {
         // Balance casted to Bigint for comparison
+        // Current account balance, CampaignFactory contract balance and total amount donated for camp1
         let accBal = await web3.eth.getBalance(accounts[0]);
         accBal = await BigInt(accBal);
         let CFBal = await web3.eth.getBalance(campaignFactoryInstance.address);
@@ -169,6 +209,9 @@ contract ("Campaign", function(accounts){
         let dAmount = await campaignInstance.getTotalDonated();
         dAmount = await BigInt(dAmount);
         
+        // Commission is set as 10%
+        // 90% of donated amount should go to the beneficiary
+        // 10% of donated amount should go to CampaignFactory contract as comission
         let commission = await dAmount / 10n ;
         let netDonationAmt = await dAmount - commission;
 
